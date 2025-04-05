e2e19b8d84a216b620700309b75a2735cef98537
Kirill Ryaska <ghettoboy2106@gmail.com>
2025-04-05 17:46:06 +0500

    fix

--- src/URLS.ts
+++ src/URLS.ts
@@ -1,3 +1,6 @@
 export const PORT = '5000'
-export const URL_SERVER = `http://localhost:${PORT}/`
-export const URL_CLIENT = `http://localhost:${PORT}/`
+// export const URL_SERVER = `http://localhost:${PORT}/`
+// export const URL_CLIENT = `http://localhost:${PORT}/`
+
+export const URL_SERVER = `songwriter-server.up.railway.app/`
+export const URL_CLIENT = `songwriter-client.up.railway.app/`
